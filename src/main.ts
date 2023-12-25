695b3320f73bb259719ef2570e1bc0f4202d227a
hsc <1825713423@qq.com>
2023-12-25 17:52:10 +0800

    1. update login.vue style 2. import animate.css

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 // main.ts
-import 'virtual:uno.css'
 // import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "@/routerPerssion"//使用路由
-import 'element-plus/theme-chalk/dark/css-vars.css'
 import "@/assets/icons/index.ts"
 import "@/assets/style/index.scss"
+
+
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'animate.css/animate.min.css' //引入
 import 'virtual:svg-icons-register'
+import 'virtual:uno.css'
+
 import diirectives from '@/directives'
 // @ts-ignore
 import App from './App.vue'
@@ -17,6 +20,7 @@ const app = createApp(App)
 app.use(diirectives);
 app.use(createPinia())
 app.use(router)
+// app.use(animated)
 
 /* for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,149 +1,250 @@
-<template>
-  <div class="login-container">
+<template class="">
+  <!-- 登录容器 -->
+  <div class=" login-container clearfix">
 
-    <div class="login-context">
-      <div class="login-left">
-        <img :src="loginSvg" style="height: 100%;width: 100%;">
-      </div>
-
-      <div class="login-right">
-
-        <!-- logo -->
-        <img :src="logo" class="logo" />
-
-        <el-divider size="large">
-          <span class="title">
-            {{ title }}
-          </span>
-        </el-divider>
-        <el-form :model="submitForm" size="large">
+    <div class="login-left"></div>
 
 
-          <el-form-item>
-            <el-input v-model="submitForm.userName" placeholder="用户账号" :prefix-icon="UserFilled" />
+    <div class="login-right flex-items-center">
+      <!-- 标题 -->
+      <img :src="logo" class="w-48px w-48px">
+
+      <el-divider border-style="dotted">
+        {{ title }}
+      </el-divider>
+
+      <div
+        class="
+        m-auto h-full w-[100%] flex flex-col items-center at-2xl:max-w-500px at-lg:max-w-500px at-md:max-w-500px at-xl:max-w-500px">
+        <h1 class="enter-x mb-3 text-center text-2xl font-bold xl:text-center xl:text-3xl">
+          登录
+        </h1>
+
+        <!-- 输入框 -->
+        <el-form :model="submitForm" size="large" class="w-full  " @submit.native.prevent>
+
+          <el-form-item prop="userName">
+            <el-input v-model="submitForm.userName" placeholder="请输入账号信息">
+              <template #prefix>
+                <svg-icon icon="ep:user"></svg-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+
+          <el-form-item prop="passWord">
+            <el-input v-model="submitForm.passWord" placeholder="请输入账号密码">
+              <template #prefix>
+                <svg-icon icon="ep:lock"></svg-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+
+          <!-- 验证码 -->
+          <el-form-item prop="code" v-show="captcha?.open">
+
+            <el-col :span="16">
+              <el-input v-model="submitForm.code" placeholder="请输入验证码" class="flex w-[70%]">
+                <template #prefix>
+                  <svg-icon icon="ep:key"></svg-icon>
+                </template>
+              </el-input>
+
+            </el-col>
+            <el-col :offset="2" :span="6">
+              <img @click="getCaptcha" :src="' data:image/gif;base64,' + captcha?.base64Img"
+                class="h-38px w-full ml-auto cursor-pointer  v-mid">
+            </el-col>
+
+
           </el-form-item>
 
           <el-form-item>
-            <el-input v-model="submitForm.passWord" placeholder="用户密码" show-password type="password"
-              :prefix-icon="Lock" />
-          </el-form-item>
 
-         <!--  <el-form-item>
-            <el-input v-model="submitForm.passWord" placeholder="用户密码" show-password type="password"
-              :prefix-icon="Lock" />
-            <img :src="logo" class="logo" />
+            <el-checkbox label="记住密码" />
+
+            <el-link type="primary" class="ml-auto">忘记密码?</el-link>
+
           </el-form-item>
- -->
 
           <el-form-item>
-            <el-button type="primary" @click="onSubmit" style="width: 100%;">
-              <span v-if="!loading">登录</span>
-              <span v-else>登录中...</span>
+            <el-button class="w-full" type="primary" @click="login">
+              登录
             </el-button>
           </el-form-item>
+
+
+          <el-divider border-style="dotted">
+            第三方登录
+          </el-divider>
+
         </el-form>
+
+        <!-- 扩展第三方 -->
+
+        <!-- 按钮组 -->
       </div>
     </div>
+
 
   </div>
 </template>
  
 <script setup lang="ts">
-import { UserFilled, Lock } from '@element-plus/icons-vue'
-import loginSvg from '@/assets/img/svg/login.png'
 import logo from '@/assets/img/logo/logo.png'
 import router from '@/router';
 import { useSettingStore } from '@/stores/setting';
 import { useUserStore } from '@/stores/user'
 // @ts-ignore
-import type { AjaxResult } from '@/utils/request/type';
-import { defineStore, storeToRefs } from 'pinia';
+import { storeToRefs } from 'pinia';
 import { onMounted, reactive, ref } from 'vue'
-import { messages } from '@/utils/message/MessageUtils';
+import { loading, messages } from '@/utils/message/MessageUtils';
 import { getCaptchaInfo } from '@/api/login/login';
+import type { Captcha } from '@/api/system/type';
 const { title } = storeToRefs(useSettingStore())
-
 const userStore = useUserStore()
-const loading = ref(false)
 
+const loadings = ref(false)
+const captcha = ref<Captcha>()
 const submitForm = reactive({
   userName: '',
   passWord: '',
+  code: '',
+  uuid: '',
 })
 
-const onSubmit = () => {
-  loading.value = true;
+// 校验对象
+const ruleFrom=ref()
+// 校验规则
+const rule=[
+  {
+
+    userName:[
+      {
+        message:'用户名称不能为空',trigger:'bluer',require:true
+      }
+    ],
+
+    passWord :[
+      {
+        message:'用户名称不能为空',trigger:'bluer',require:true
+      }
+    ],
+
+
+  }
+]
+
+const login = () => {
+  loadings.value = true;
+  loading.open()
   userStore.login({
     userName: submitForm.userName.trim(),
     passWord: submitForm.passWord.trim()
   }).then(res => {
-    loading.value = false;
+    loadings.value = false;
     router.push("/")
-  }).catch(error=>{
+  }).catch(error => {
     messages.error(error.msg)
     userStore.$clearCache();// 清空缓存中token
     userStore.$resetOauth2();//包括pinia中Oauth2对象
-    loading.value = false;
-    
+    loadings.value = false;
+  }).finally(() => {
+    loading.close()
+  }
+
+  )
+}
+
+const getCaptcha = () => {
+  getCaptchaInfo().then(res => {
+
+    captcha.value = res.data
+    // 判断是否开启pro,未开启直接赋值
+    if (captcha.value.activeProfile !== 'pro') {
+      submitForm.uuid = captcha.value.result
+    }
+
+
+
   })
 }
-onMounted(()=>{
-  initCaptcha();
+
+onMounted(() => {
+  getCaptcha();
 })
 
-const initCaptcha=()=>{
-  getCaptchaInfo().then(res=>{
-
-  })
-}
 
 
 </script>
  
+
+<!-- 
+  https://juejin.cn/post/7004622232378966046#heading-14
+  https://juejin.cn/post/7004622232378966046
+
+彻底理解CSS Flexbox布局，看这一篇就够了
+
+main axis: Flex 父元素的主轴是指子元素布局的主要方向轴，它由属性flex-direction来确定主轴是水平还是垂直的，默认为水平轴。
+main-start & main-end: 分别表示主轴的开始和结束，子元素在父元素中会沿着主轴从main-start到main-end排布。
+main size: 单个项目占据主轴的长度大小。
+cross axis: 交叉轴，与主轴垂直。
+cross-start & cross-end: 分别表示交叉轴的开始和结束。子元素在交叉轴的排布从cross-start开始到cross-end。
+cross size: 子元素在交叉轴方向上的大小。
+
+
+    1. flex 块状元素,inlinie-flex行内元素
+    2. flex-direction: row | row-reverse | column | column-reverse; row从左向右 row-reverse 从右向左 column 从上向下 column-reverse取反
+}
+-->
 <style lang="scss" scoped>
-$login-container-color: #fdfdfd;
+// $login-container-color: #fdfdfd;
+.clearfix:after {
+  visibility: hidden;
+  display: block;
+  font-size: 0;
+  content: " ";
+  clear: both;
+  height: 0;
+}
 
 .login-container {
   width: 100%;
-  height: 100%;
-  position: relative;
-  background-color: $login-container-color;
+  height: calc(100vh);
+  // position: relative;
+  display: flex;
+  flex-direction: row; // 从左向右
+  // background-color: $login-container-color;
 
-  // 内容
-  .login-context {
-    width: calc(100% - 200px);
-    height: calc(100% - 100px);
-    position: absolute;
-    margin: auto;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    box-shadow: 0.1px 0.1px 5px #000000;
-    border-radius: 10px;
-
-    .login-left {
-      width: 50%;
-      height: 100%;
-      float: left;
-    }
-
-    .login-right {
-      float: left;
-      width: 50%;
-      height: 100%;
-      padding: 50px;
-
-      .logo {
-        width: 50px;
-        height: 50px;
-        // margin: auto;
-      }
-
-      .title {}
-
-    }
+  .login-left {
+    background: url('@/assets/img/svg/login.png');
+    flex-basis: 50%;
   }
 
+  .login-right {
+    order: 1;
+    display: flex;
+    flex-basis: 50%;
+    flex-direction: column; //上下布局
+    transition: transform 0.3s ease;
+  }
+
+
+}
+
+.container {
+  display: flex;
+  animation: slideLeft 0.3s ease;
+}
+
+// 当元素680px自动隐藏化
+@media (max-width: 680px) {
+
+  .login-container .login-left {
+    display: none;
+  }
+
+  .login-container .login-right {
+    flex-basis: 100%;
+  }
 }
 </style>
