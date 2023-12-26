6fa67cf4f45e8f307a4c8c672af8cdc10530b2c0
hsc <1825713423@qq.com>
2023-12-26 19:10:42 +0800

    tips: 合并前提交--忽略

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -6,7 +6,6 @@
 export {}
 declare global {
   const EffectScope: typeof import('vue')['EffectScope']
-  const ElIconsvgIcon: typeof import('@element-plus/icons-vue')['svgIcon']
   const computed: typeof import('vue')['computed']
   const createApp: typeof import('vue')['createApp']
   const customRef: typeof import('vue')['customRef']
